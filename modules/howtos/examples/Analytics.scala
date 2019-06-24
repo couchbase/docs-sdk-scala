193746c2d32a8366c52fbed0f38d331b6485a83e
Graham Pople <grahampople@gmail.com>
2019-06-24 14:10:36 +0100

    Analytics docs

---
+++ modules/howtos/examples/Analytics.scala
@@ -0,0 +1,158 @@
+// #tag::imports[]
+import com.couchbase.client.scala._
+import com.couchbase.client.scala.analytics.{AnalyticsOptions, AnalyticsResult, ReactiveAnalyticsResult}
+import com.couchbase.client.scala.json._
+import reactor.core.scala.publisher.{Flux, Mono}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+// #end::imports[]
+
+
+object ErrorHandling {
+
+  // #tag::cluster[]
+  val cluster = Cluster.connect("localhost", "username", "password")
+  // #end::cluster[]
+
+  // #tag::resources[]
+  val bucket = cluster.bucket("bucket-name")
+  val scope = bucket.scope("scope-name")
+  val collection = scope.collection("collection-name")
+  // #end::resources[]
+
+  // #tag::apis[]
+  val asyncApi: AsyncCollection = collection.async
+  val reactiveApi: ReactiveCollection = collection.reactive
+  // #end::apis[]
+
+  def simple() {
+    // #tag::simple[]
+    val query = """select "hello" as greeting;"""
+    val result: Try[AnalyticsResult] = cluster.analyticsQuery(query)
+    val rows: Try[Seq[JsonObject]] = result.flatMap(_.allRowsAs[JsonObject])
+
+    rows match {
+      case Success(r: Seq[JsonObject]) => println(s"Row: ${r.head}")
+      case Failure(err) => println(s"Failure ${err}")
+    }
+    // #end::simple[]
+  }
+
+  def simpleBetter() {
+    // #tag::simple-better[]
+    cluster.analyticsQuery("""select "hello" as greeting;""")
+      .flatMap(_.allRowsAs[JsonObject]) match {
+      case Success(r)   => println(s"Row: ${r.head}")
+      case Failure(err) => println(s"Failure ${err}")
+    }
+    // #end::simple-better[]
+  }
+
+  def parameterised() {
+    // #tag::parameterised[]
+    cluster.analyticsQuery(
+      """select airportname, country from airports where country = ?;""",
+      AnalyticsOptions().positionalParameters("France"))
+      .flatMap(_.allRowsAs[JsonObject]) match {
+      case Success(r)   => r.foreach(row => println(s"Row: ${row}"))
+      case Failure(err) => println(s"Failure ${err}")
+    }
+    // #end::parameterised[]
+  }
+
+  def named() {
+    // #tag::named[]
+    cluster.analyticsQuery(
+      """select airportname, country from airports where country = $country;""",
+      AnalyticsOptions().namedParameters("country" -> "France"))
+      .flatMap(_.allRowsAs[JsonObject]) match {
+      case Success(r)   => r.foreach(row => println(s"Row: ${row}"))
+      case Failure(err) => println(s"Failure ${err}")
+    }
+    // #end::named[]
+  }
+
+  def parameters() {
+    // #tag::parameters[]
+    cluster.analyticsQuery(
+      """select airportname, country from airports where country = "France";""",
+      AnalyticsOptions()
+        // Ask the analytics service to give this request higher priority
+        .priority(true)
+
+        // The client context id is returned in the results, so can be used by the
+        // application to correlate requests and responses
+        .clientContextId("my-id")
+
+        // Override how long the analytics query is allowed to take before timing out
+        .timeout(90.seconds)
+    ) match {
+      case Success(r: AnalyticsResult) =>
+        assert(r.meta.clientContextId.contains("my-id"))
+      case Failure(err) => println(s"Failure ${err}")
+    }
+  }
+  // #end::parameters[]
+
+  def metrics() {
+    // #tag::metrics[]
+    val stmt =
+      """select airportname, country from airports where country = "France";"""
+    cluster.analyticsQuery(stmt) match {
+      case Success(result) =>
+        result.meta.metrics.foreach(metrics => {
+          println(s"Elapsed: ${metrics.elapsedTime}")
+          println(s"Results: ${metrics.resultCount}")
+          println(s"Errors:  ${metrics.errorCount}")
+        })
+      case Failure(err) => println(s"Failure ${err}")
+    }
+    // #end::metrics[]
+
+  }
+
+
+  def async() {
+    // #tag::async[]
+    // When we work with Scala Futures an ExecutionContext must be provided.
+    // For this example we'll just use the global default
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    val stmt = """select airportname, country from airports where country = "France";"""
+    val future: Future[AnalyticsResult] = cluster.async.analyticsQuery(stmt)
+
+    future onComplete {
+      case Success(result) =>
+        result.allRowsAs[JsonObject] match {
+          case Success(rows) => rows.foreach(println(_))
+          case Failure(err) => println(s"Error: $err")
+        }
+      case Failure(err) => println(s"Error: $err")
+    }
+    // #end::async[]
+  }
+
+  def reactive() {
+    // #tag::reactive[]
+    val stmt =
+      """select airportname, country from airports where country = "France";"""
+    val mono: Mono[ReactiveAnalyticsResult] = cluster.reactive.analyticsQuery(stmt)
+
+    val rows: Flux[JsonObject] = mono
+      // ReactiveQueryResult contains a rows: Flux[AnalyticsRow]
+      .flatMapMany(result => result.rowsAs[JsonObject])
+
+    // Just for example, block on the rows.  This is not best practice and apps
+    // should generally not block.
+    val allRows: Seq[JsonObject] = rows
+      .doOnNext(row => println(row))
+      .doOnError(err => println(s"Error: $err"))
+      .collectSeq()
+      .block()
+
+    // #end::reactive[]
+  }
+
+}
